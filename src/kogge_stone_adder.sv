714f72542dc7e851f2bdf77ce1698ce82904df8b
Darshan <radarshan15@gmail.com>
2026-04-25 14:42:54 +0530

    Refactor kogge_stone_adder for improved clarity

--- src/kogge_stone_adder.sv
+++ src/kogge_stone_adder.sv
@@ -1,51 +1,55 @@
+`default_nettype none
+
 module kogge_stone_adder #(
     parameter width = 8
-) (
-    input logic [width-1:0] a,b,
-    input logic cin,
-    output logic [width-1:0] sum,
-    output logic cout
+)(
+    input  wire [width-1:0] a,
+    input  wire [width-1:0] b,
+    input  wire cin,
+    output wire [width-1:0] sum,
+    output wire cout
 );
 
-    localparam final_stage = $clog2(width);
+    localparam final_stage = 3; // log2(8)=3 (avoid $clog2 issues)
 
-    logic [width - 1:0] propagate [final_stage:0];
-    logic [width - 1:0] gen       [final_stage:0];
+    wire [width-1:0] propagate [0:final_stage];
+    wire [width-1:0] gen       [0:final_stage];
 
-    // stage - 1 
+    // Stage 0
+    assign propagate[0] = a ^ b;
+    assign gen[0]       = a & b;
 
-    assign propagate [0] = a ^ b;                                                  // for checking the propagate 
-    assign gen [0] [0] = (a[0] & b[0]) | (propagate [0] [0] & cin);               // to add cin 
-    assign gen [0] [width - 1 : 0]= a[width - 1 : 0] & b [width - 1 : 0];         // to check the generate 
+    // include cin
+    assign gen[0][0] = gen[0][0] | (propagate[0][0] & cin);
 
-    // stage - 2
-
-    genvar current_stage,bit_position;
+    genvar i, j;
 
     generate
-        for ( current_stage = 1 ; current_stage <= final_stage ; current_stage++ ) begin : stage_loop
-            localparam int jump = 1 << (current_stage -1);
-            for (bit_position = 0 ; bit_position < width ; bit_position++ ) begin : calculation_loop 
-                if (bit_position >= jump) begin       // safe condition for calculating gen and propagate
-                    assign gen [current_stage] [bit_position] = gen [current_stage - 1] [bit_position] | propagate [current_stage - 1] [bit_position] & gen [current_stage - 1] [bit_position - jump];
-                    assign propagate [current_stage] [bit_position] = propagate [current_stage - 1] [bit_position] & propagate [current_stage - 1] [bit_position - jump];
-                end 
-                else begin    // if the grouping hit 0 index
-                    assign gen [current_stage] [bit_position] = gen [current_stage - 1] [bit_position];
-                    assign propagate [current_stage] [bit_position] = propagate [current_stage - 1] [bit_position];
+        for (i = 1; i <= final_stage; i = i + 1) begin : stage_loop
+            for (j = 0; j < width; j = j + 1) begin : bit_loop
+                if (j >= (1 << (i-1))) begin
+                    assign gen[i][j] =
+                        gen[i-1][j] |
+                        (propagate[i-1][j] & gen[i-1][j - (1 << (i-1))]);
+
+                    assign propagate[i][j] =
+                        propagate[i-1][j] &
+                        propagate[i-1][j - (1 << (i-1))];
+                end else begin
+                    assign gen[i][j]       = gen[i-1][j];
+                    assign propagate[i][j] = propagate[i-1][j];
                 end
             end
-            
         end
     endgenerate
 
-    // stage - 3
+    // Sum
+    assign sum[0] = propagate[0][0] ^ cin;
 
-    assign sum [0]             = propagate [0] [0] ^ cin;
+    assign sum[width-1:1] =
+        propagate[0][width-1:1] ^
+        gen[final_stage][width-2:0];
 
-    assign sum [width - 1 : 1] = propagate [0] [width - 1:1] ^ gen [final_stage] [width - 2:0];
+    assign cout = gen[final_stage][width-1];
 
-    assign cout                = gen[final_stage] [width - 1];
-
-
-endmodule 
+endmodule
