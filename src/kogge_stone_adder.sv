72ce9252af48a16948455d3c6951ef62cafb9a6a
Darshan <radarshan15@gmail.com>
2026-04-25 15:30:27 +0530

    Refactor kogge_stone_adder to use logic types

--- src/kogge_stone_adder.sv
+++ src/kogge_stone_adder.sv
@@ -2,59 +2,46 @@
 
 module kogge_stone_adder #(
     parameter width = 8
-)(
-    input  wire [width-1:0] a,
-    input  wire [width-1:0] b,
-    input  wire cin,
-    output wire [width-1:0] sum,
-    output wire cout
+) (
+    input  logic [width-1:0] a, b,
+    input  logic             cin,
+    output logic [width-1:0] sum,
+    output logic             cout
 );
 
-    localparam final_stage = 3; // log2(8)=3 (avoid $clog2 issues)
+    localparam final_stage = $clog2(width);
 
-    wire [width-1:0] propagate [0:final_stage];
-    wire [width-1:0] gen       [0:final_stage];
+    logic [width - 1:0] propagate [final_stage:0];
+    logic [width - 1:0] gen       [final_stage:0];
 
-    // Stage 0
-    wire [width-1:0] gen0_raw;
+    // stage - 1 
+    assign propagate [0] = a ^ b;                                                
+    assign gen [0][0] = (a[0] & b[0]) | (propagate[0][0] & cin);               
+    // FIX: Changed index from [width-1:0] to [width-1:1] to prevent multiple drivers on bit 0
+    assign gen [0][width-1:1] = a[width-1:1] & b[width-1:1];          
 
-assign propagate[0] = a ^ b;
-assign gen0_raw     = a & b;
-
-// properly include cin without multiple drivers
-assign gen[0] = gen0_raw | (propagate[0] & { {width-1{1'b0}}, cin });
-
-    // include cin
-    assign gen[0][0] = gen[0][0] | (propagate[0][0] & cin);
-
-    genvar i, j;
+    // stage - 2
+    genvar current_stage, bit_position;
 
     generate
-        for (i = 1; i <= final_stage; i = i + 1) begin : stage_loop
-            for (j = 0; j < width; j = j + 1) begin : bit_loop
-                if (j >= (1 << (i-1))) begin
-                    assign gen[i][j] =
-                        gen[i-1][j] |
-                        (propagate[i-1][j] & gen[i-1][j - (1 << (i-1))]);
-
-                    assign propagate[i][j] =
-                        propagate[i-1][j] &
-                        propagate[i-1][j - (1 << (i-1))];
-                end else begin
-                    assign gen[i][j]       = gen[i-1][j];
-                    assign propagate[i][j] = propagate[i-1][j];
+        for (current_stage = 1; current_stage <= final_stage; current_stage++) begin : stage_loop
+            localparam int jump = 1 << (current_stage - 1);
+            for (bit_position = 0; bit_position < width; bit_position++) begin : calculation_loop 
+                if (bit_position >= jump) begin       
+                    assign gen [current_stage][bit_position] = gen [current_stage - 1][bit_position] | (propagate [current_stage - 1][bit_position] & gen [current_stage - 1][bit_position - jump]);
+                    assign propagate [current_stage][bit_position] = propagate [current_stage - 1][bit_position] & propagate [current_stage - 1][bit_position - jump];
+                end 
+                else begin    
+                    assign gen [current_stage][bit_position] = gen [current_stage - 1][bit_position];
+                    assign propagate [current_stage][bit_position] = propagate [current_stage - 1][bit_position];
                 end
             end
         end
     endgenerate
 
-    // Sum
-    assign sum[0] = propagate[0][0] ^ cin;
-
-    assign sum[width-1:1] =
-        propagate[0][width-1:1] ^
-        gen[final_stage][width-2:0];
-
-    assign cout = gen[final_stage][width-1];
+    // stage - 3
+    assign sum [0]             = propagate [0][0] ^ cin;
+    assign sum [width - 1 : 1] = propagate [0][width - 1:1] ^ gen [final_stage][width - 2:0];
+    assign cout                = gen[final_stage][width - 1];
 
 endmodule
